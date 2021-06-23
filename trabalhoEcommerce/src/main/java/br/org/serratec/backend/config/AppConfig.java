801d67e9a34adf8f9a7cadedcad108dd207f8642
VanessaWendling <vanessamw2001@hotmail.com>
2021-06-23 18:56:30 -0300

    CORS

--- trabalhoEcommerce/src/main/java/br/org/serratec/backend/config/AppConfig.java
+++ trabalhoEcommerce/src/main/java/br/org/serratec/backend/config/AppConfig.java
@@ -3,13 +3,26 @@ package br.org.serratec.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 //essa classe vai gerenciar a senha dos usuarios
 @Configuration
-public class AppConfig {
+public class AppConfig implements WebMvcConfigurer{
 	
 	@Bean
 	public BCryptPasswordEncoder bCryptyPasswordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+	
+	@Override
+    public void addCorsMappings(CorsRegistry registry) {
+
+        registry.addMapping("/**")
+                .allowedOrigins(
+                        "http://localhost:3000/")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD")
+                .allowCredentials(true)
+        ;
+    }
 }
